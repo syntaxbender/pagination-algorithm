78858bcf15552faf046aa3133e0bde16b73d5c98
Syntax Bender <mail@syntaxbender.com>
2021-03-26 01:46:31 +0300

    js added

--- base.php
+++ base.php
@@ -4,29 +4,29 @@
 	$currentpage = 19;
 	$string = $currentpage;
 	$lastPage = 20;
-	$kayma = 0;
+	$slip = 0;
 	$indicatorStopper = 0;
 	// 0 negative direction
 	// 1 pozitive direction
-	while($step <= 10){ // totalde 7 buton olacaktır.
-		$degisim = ($indicatorStopper == 0) ? ceil($step/2) : $step;
+	while($step <= 10){ // totalde 11 buton olacaktır.
+		$addition = ($indicatorStopper == 0) ? ceil($step/2) : $step;
 		if($directionIndicator == 0){
-			$pageNum = $currentpage-$degisim+$kayma;
+			$pageNum = $currentpage-$addition+$slip;
 			if($indicatorStopper == 1 && $pageNum<1) break;
 			if($pageNum<1){
 				$indicatorStopper = 1;
 				$directionIndicator = ($directionIndicator+1)%2;
-				$kayma = (is_int($step/2) === false) ? (ceil($step/2)-1) : 0;
+				$slip = (is_int($step/2) === false) ? (ceil($step/2)-1) : 0;
 				continue;
 			}
 			$string = $pageNum." ".$string;
 		}else{
-			$pageNum = $currentpage+$degisim-$kayma;
+			$pageNum = $currentpage+$addition-$slip;
 			if($indicatorStopper == 1 && $pageNum>$lastPage) break;
 			if($pageNum>$lastPage){
 				$indicatorStopper = 1;
 				$directionIndicator = ($directionIndicator+1)%2;
-				$kayma = (is_int($step/2)) ? (ceil($step/2)-1) : 0;
+				$slip = (is_int($step/2)) ? (ceil($step/2)-1) : 0;
 				continue;
 			}
 			$string = $string." ".$pageNum;
